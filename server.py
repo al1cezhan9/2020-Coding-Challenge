21a01f7ddfa7f983499734c3746cb4979b75d469
al1cezhan9 <alice.az.zhang@gmail.com>
2025-03-14 12:15:43 -0400

    sorts teams by score

--- server.py
+++ server.py
@@ -51,6 +51,9 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    # sorting the teams by non-decreasing score before returning
+    scoreboard.sort(key=lambda team: team["score"], reverse=True)
+
     return jsonify(scoreboard=scoreboard)
 
 
